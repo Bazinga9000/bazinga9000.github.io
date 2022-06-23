fcf2c9969001f4126b31259b673bc0f8a08624d6
Bazinga9000 <bazonkidonk@gmail.com>
2022-06-22 21:43:10 -0400

    bump argam names to 490, modernize

--- docs/js/argam/argam.js
+++ docs/js/argam/argam.js
@@ -1,95 +1,8 @@
-const argamAlphabet = String.fromCharCode(...Array.from({length: 480}, (_, i) => 0xE000 + i));
+const argamAlphabet = String.fromCharCode(...Array.from({length: 490}, (_, i) => 0xE000 + i));
 
 BigNumber.config({ ALPHABET: argamAlphabet });
 
-const digitNames = [
-    'Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight',
-    'Nine', 'Dess', 'Ell', 'Zen', 'Thise', 'Zeff', 'Trick', 'Tess', 'Zote', 'Dine', 'Ax', 'Score',
-    'Tress', 'Dell', 'Flore', 'Cadex', 'Quint', 'Dithe', 'Trine', 'Caven', 'Neve', 'Kinex',
-    'Sode', 'Twive', 'Trell', 'Dote', 'Kineff', 'Exent', 'Mack', 'Dax', 'Trithe', 'Kinoct',
-    'Lume', 'Exeff', 'Sill', 'Cadell', 'Kinove', 'Diore', 'Foss', 'Exoct', 'Effent', 'Kiness',
-    'Trizote', 'Cadithe', 'Sull', 'Exove', 'Kinell', 'Sevoct', 'Triax', 'Deve', 'Clore', 'Shock',
-    'Ark', 'Disode', 'Senove', 'Octent', 'Kinithe', 'Exell', 'Kale', 'Cazote', 'Triore', 'Sevess',
-    'Calse', 'Octove', 'Scand', 'Dimack', 'Kinchick', 'Catax', 'Sevell', 'Exithe', 'Tite', 'Octess',
-    'Novent', 'Dilume', 'Van', 'Sezzen', 'Kinote', 'Dill', 'Trinue', 'Octell', 'Crome', 'Novess',
-    'Sevithe', 'Cadore', 'Trisode', 'Doss', 'Kinax', 'Extess', 'Mang', 'Seneff', 'Novell', 'Dessent',
-    'Ferr', 'Exote', 'Cobe', 'Octithe', 'Setrick', 'Disull', 'Nick', 'Catrine', 'Cupe', 'Desell',
-    'Trimack', 'Setess', 'Zinn', 'Exax', 'Kinore', 'Cadeve', 'Novithe', 'Diclore', 'Sevote', 'Hund',
-
-    /*
-  // dear reader of my source code:
-  // the following may or may not be correct, to be honest
-  // i found no official resources for the ismarragam naming scheme above 120
-  // believe it or not, i got these from some random wikipedia that goes into
-  // >800. thankfully, the font only goes up to 480 so i don't have to deal with
-  // the creator of this list's terrible choice in naming primes after oganesson
-  //
-  // i got these from here, if you're really curious
-  // https://ex515.fandom.com/wiki/List_of_numbers
-  // nb: the page will say any prime >= 401 has an incorrect atomic number.
-  // the page is wrong. i checked myself.
-  //
-  // if you, dear reader, find a better source, please just submit an issue on
-  // the github repo for this website at
-  // https://github.com/Bazinga9000/bazinga9000.github.io
-  //
-  // There was also at least one error in the page, the one that i caught:
-  // digit 477 (3×3×53) was erroniously given the name "noclore" (3×3×59). I
-  // have changed this to "nosull", as it should be. Unsure how many other errors
-  // there are.
-  // Exark and Sevark (6×61, 7×61) were erroniously spelled with "g", that has been
-  // fixed too.
-  // if you happen to find another error, submit an issue and i will be eternally
-  // greatful
-  //
-  */
-
-    'Ellent', 'Diark', 'Trilume', 'Casode', 'Kincue', 'Sedine', 'Gall', 'Tweven',
-    'Trisill', 'Desithe', 'Gerr', 'Ellzen', 'Sevax', 'Dikale', 'Kintrine', 'Octote',
-    'Arsen', 'Exflore', 'Selene', 'Sescore', 'Trifoss', 'Dicalse', 'Ellithe', 'Zenent',
-    'Kineve', 'Discand', 'Effintri', 'Camack', 'Brome', 'Exquint', 'Krypt', 'Octax',
-    'Nozote', 'Ellzeff', 'Kinsode', 'Zenithe', 'Rube', 'Ditite', 'Trisull', 'Kintrive',
-    'Seflore', 'Extrine', 'Stront', 'Calume', 'Elltrick', 'Divan', 'Yttr', 'Cadexeff',
-    'Thisent', 'Desote', 'Novax', 'Casill', 'Zirk', 'Exneve', 'Sequint', 'Elltess',
-    'Triclore', 'Dicrome', 'Niobe', 'Scorove', 'Molyb', 'Zeffithe', 'Triark', 'Octflore',
-    'Kinmack', 'Exsode', 'Ellzote', 'Cafoss', 'Setrine', 'Dessax', 'Technes', 'Tritwex',
-    'Ruthe', 'Dimang', 'Trickithe', 'Zeffent', 'Rhode', 'Elldine', 'Pallade', 'Ocquint',
-    'Trikale', 'Diferr', 'Seneve', 'Zenzote', 'Kinlume', 'Dicobe', 'Noflore', 'Thitess',
-    'Ellax', 'Kinexeff', 'Argene', 'Casull', 'Tricalse', 'Dinick', 'Kinsill', 'Excue',
-    'Sesode', 'Dicupe', 'Triscand', 'Scorell', 'Thizote', 'Exmack', 'Cadme', 'Setwive',
-    'Trickent', 'Dizinn', 'Inde', 'Zenax', 'Stane', 'Desflore', 'Elltress', 'Octneve',
-    'Stibe', 'Thidine', 'Kinfoss', 'Caclore', 'Tritite', 'Zeffzote', 'Tellure', 'Kinexoct',
-    'Iode', 'Ellindi', 'Thrive', 'Cadark', 'Effinkin', 'Exlume', 'Thisax', 'Octsode',
-    'Trivan', 'Desquint', 'Xene', 'Exineff', 'Ellflore', 'Digall', 'Trickote', 'Tweight',
-    'Caese', 'Exsill', 'Semack', 'Scorithe', 'Noneve', 'Digerr', 'Bare', 'Cadexell',
-    'Kinsull', 'Zeffax', 'Tricrome', 'Cakale', 'Lanthe', 'Kinexove', 'Cere', 'Tresszote',
-    'Thitress', 'Diarsen', 'Ellquint', 'Zenflore', 'Prase', 'Diselene', 'Nosode', 'Kinsevoct',
-    'Neode', 'Exfoss', 'Promethe', 'Cacalse', 'Trickax', 'Thidell', 'Selume', 'Notwive',
-    'Zotent', 'Desneve', 'Trimang', 'Cascand', 'Samare', 'Effinex', 'Kinclore', 'Octmack',
-    'Elltrine', 'Dibrome', 'Thiflore', 'Scortrick', 'Sesill', 'Dikrypt', 'Triferr', 'Tessax',
-    'Kinark', 'Zotdine', 'Europ', 'Cadsevell', 'Tricobe', 'Dessode', 'Gadole', 'Cadexithe',
-    'Terbe', 'Dirube', 'Kinsenove', 'Catite', 'Dyspre', 'Exsull', 'Ellneve', 'Kintwex',
-    'Trinick', 'Zefflore', 'Zotax', 'Dinent', 'Thiquint', 'Distront', 'Tricupe', 'Octlume',
-    'Sefoss', 'Kinexell', 'Holme', 'Cavan', 'Nomack', 'Diyttr', 'Kinkale', 'Exsevoct',
-    'Erbe', 'Thisindi', 'Trizinn', 'Scorzote', 'Ellsode', 'Dinax', 'Effcue', 'Octsill',
-    'Trickflore', 'Dizirk', 'Thule', 'Zeneve', 'Ytterbe', 'Zeffquint', 'Thitrine', 'Elltwive',
-    'Lutete', 'Exclore', 'Kincalse', 'Cacrome', 'Zotress', 'Diniobe', 'Hafne', 'Kinoctove',
-    'Axent', 'Dimolyb', 'Ellintri', 'Cadsevithe', 'Kinscand', 'Exark', 'Tante', 'Tressflore',
-    'Nolume', 'Desmack', 'Sesull', 'Zensode', 'Wolfre', 'Zotdell', 'Trickquint', 'Octfoss',
-    'Thineve', 'Exsenove', 'Rhene', 'Scorax', 'Trigall', 'Ditechness', 'Osme', 'Tritweven',
-    'Kinsevell', 'Diruthe', 'Nosill', 'Camang', 'Iride', 'Kinexithe', 'Zotflore', 'Effinoct',
-    'Trigerr', 'Dirhode', 'Kintite', 'Exinell', 'Plate', 'Dipallade', 'Axtress', 'Scorent',
-    'Aure', 'Exkale', 'Thisode', 'Caferr', 'Novinkin', 'Zeffneve', 'Ellmack', 'Cadexote',
-    'Hydrare', 'Deslume', 'Triarsen', 'Cacobe', 'Seclore', 'Dinflore', 'Kinvan', 'Thitwive',
-    'Triselene', 'Axdell', 'Thale', 'Scoretress', 'Plumb', 'Diargene', 'Nofoss', 'Octsull',
-    'Zotquint', 'Excalse', 'Sevark', 'Canick', 'Thitrell', 'Desill', 'Bisme', 'Tesstrine',
-    'Polone', 'Zeffsode', 'Trickneve', 'Cacupe', 'Axflore', 'Exscand', 'Astate', 'Kinoctell',
-    'Tressent', 'Zotdithe', 'Rad', 'Zenmack', 'Kincrome', 'Dicadme', 'Tribrome', 'Setwex',
-    'Frane', 'Trickindi', 'Ellume', 'Cazinn', 'Trikrypt', 'Dinde', 'Kinsevithe', 'Cadexax',
-    'Rade', 'Distane', 'Zotrine', 'Scorflore', 'Actin', 'Exsevell', 'Thore', 'Tessneve',
-    'Tricksode', 'Distibe', 'Prote', 'Exinithe', 'Sekale', 'Desfoss', 'Trirube', 'Octclore',
-    'Ellsill', 'Extite', 'Axquint', 'Cadsevote', 'Nosull', 'Ditellure', 'Uran'
-];
+const digitNames = "zero one two three four five six seven eight nine dess ell zen thise zeff trick tess zote dine ax score tress dell flore cadex quint dithe trine caven neve kinex sode twive trell dote kineff exent mack dax trithe kinoct lume exeff sill cadell kinove diore foss exoct effent kiness trote cadithe sull exove kinell sevoct trax deve clore shock ark disode senove twex kinithe exell kale cazote triore sevess calse octove scand dimack kinchick catax sevell exithe tite kintess novent dilume van sezzen kinote dill treve octell crome novess sevithe cadore trisode doss kinax ozzen mang seneff novell kent ferr exote cobe octithe setrick disull nick catrine cupe desell trimack setess zinn exax kinore caneve novithe diclore sevote hund ellent diark trilume casode kincue disenove gall tweven trisill desithe gerr ellzen sevax dikale kintrine octote arsen exore selene dezeff tross dalse ellithe zenent kineve discand sentress camack brome exquint krypt octax novote ellzeff kinsode zenithe rube dite trisull kintwive seflore extrine stron calume trikinell divan yttr zenzeff thisent dessote novax casill zirc exeve sequint elltess triclore dicrome niobe zenchick molyb thizeff triark offlore kinmack exsode ellote cafoss setrine dessax technes zentess ruthe dimang trickithe zeffent rhode elldine pallade descore trikale diferr seneve zenote kinlume dicobe noveflore thitess ellax kinexeff argene cadsull trialse dinick kinsill excue sesode dicupe triscand cadkinell thiote exmack cadme setwive trickent dinn inde zenax stann desore trisevell octeve stibbe thidine kinoss caclore trite zeffote tellure trikintess iode elldell novetrine cadark effinkin exume thiax octsode trivan desquint xeen exineff ellore digall trickote tweight caese exill semack cadkinithe noneve digerr bare cadexell kinsull zeffax tricrome cakale lanthe destrine cere tessote trisevithe diarsen ellquint zenore prase diselene novesode kinsevoct neo exfoss prome cacalse trikinax thidell selume novetwive zotent desneve trimang cascand samare effinex kinclore octmack elltrine dibrome thiore zenquint sesill dikrypt triferr tessax kinark dinovote europ cadsevell tricobe desode gadole cadexithe terbe dirube kinsenove catite dyss exsull ellneve kintwex trinick zefflore zotax dinent thiquint distron tricupe octlume sefoss kinexell holme cadvan novemack ditt kinkale testress erbe thidithe trinn cadkinote ellsode dinovax effcue octsill trikinore dirc thume zeneve ytterb kinsevess thitrine elltwive lute exclore kincalse cacrome trisevote diobe hafne kinoctove axent dimolyb elltrell cadsevithe kinscand exark tante tessore novelume desmack sesull zensode wolfre dellote chickquint octfoss thineve zeftrine rhene cadkinax trigall ditechnes osme tritweven kinsevell diruthe novesill camang iride kinexithe zotore effinoct trigerr dirhode kintite exinell platt dipallade trisevax scorent aure exkale thisode caferr novinkin zeffneve ellmack cadexote hyde deslume triarsen cacobe seclore dinoveflore kinvan thitwive triselene axdell thule kinsezzen plumb diargene novefoss octsull zotquint exalse sevark canick thitrell dessill biss testrine polone zeffsode trikineve cacupe axore exscand aste kinoctell tressent thidote nite zenmack kincrome dicadme tribrome setwex fran kinovess ellume cazinn trikrypt dyinde kinsevithe cadexax rade distann zotrine cadkinore acte exsevell thor tesseve trikinsode distibbe prote exinithe sekale desfoss trirube octclore ellsill extite axquint cadsevote novesull ditellure urane kinozzen thimack dyiode triseflore dellent kinmang dinovetrine nept octark tristron".split(" ");
 
 console.log(digitNames.length);
 
@@ -127,6 +40,15 @@ function getCSSClass(num, base) {
 
     return 'argam-large';
 }
+
+function toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+    );
+  }
 
 function toArgamString(num, base) {
     const n = new BigNumber(num);
@@ -189,5 +111,5 @@ function toArgamName(num, base) {
         name += parse_suffix('One', suffix) + ' ';
     }
 
-    return name.slice(0, -1);
+    return toTitleCase(name.slice(0, -1));
 }
